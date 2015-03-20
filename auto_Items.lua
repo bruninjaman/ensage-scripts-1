a23f19ae98268af192675d2a2113252c9d9d5d4f
K L A U S N O M I <theklausnomi@gmail.com>
2015-03-21 02:04:23 +0800

    v

--- auto_Items.lua
+++ auto_Items.lua
@@ -34,11 +34,11 @@ function Tick(tick)
 
 	if me.alive and not me:IsInvisible() and not me:IsChanneling() then
 
-		if enableQuickbuy and me.health/me.maxHealth < 0.09 then
+		if enableQuickbuy and me.health/me.maxHealth < 0.07 then
 			client:ExecuteCmd("dota_purchase_quickbuy")
 		end
 
-		if enableBloodstone and bloodstone and bloodstone:CanBeCasted() and me.health/me.maxHealth < 0.04 then
+		if enableBloodstone and bloodstone and bloodstone:CanBeCasted() and me.health/me.maxHealth < 0.02 then
 			me:CastAbility(bloodstone,me.position)
 			Sleep(1000)
 		end
