a6d2a155ae7feefa4bcadda966fcc10b01628cdf
K L A U S N O M I <theklausnomi@gmail.com>
2015-03-21 06:40:27 +0800

    V

--- take_rune.lua
+++ take_rune.lua
@@ -1,35 +1,45 @@
 require("libs.DrawManager3D")
+require("libs.ScriptConfig")
 
-local key = string.byte("M")
+config = ScriptConfig.new()
+config:SetParameter("Hotkey", "M", config.TYPE_HOTKEY)
+config:Load()
 
-local myId = nil local play = false local activated = true
+toggleKey = config.Hotkey
 
-local rec1 = drawMgr3D:CreateRect(Vector(-2272,1792,0), Vector(0,0,0), Vector2D(0,0), Vector2D(30,30), 0x000000ff, drawMgr:GetTextureId("NyanUI/other/fav_heart")) rec1.visible = false
-local rec2 = drawMgr3D:CreateRect(Vector(3000,-2450,0), Vector(0,0,0), Vector2D(0,0), Vector2D(30,30), 0x000000ff, drawMgr:GetTextureId("NyanUI/other/fav_heart")) rec2.visible = false
+local Play = false
+
+local rec1 = drawMgr3D:CreateRect(Vector(-2272,1792,0), Vector(0,0,0), Vector2D(0,0), Vector2D(30,30), 0x000000ff, drawMgr:GetTextureId("NyanUI/other/fav_heart"))
+local rec2 = drawMgr3D:CreateRect(Vector(3000,-2450,0), Vector(0,0,0), Vector2D(0,0), Vector2D(30,30), 0x000000ff, drawMgr:GetTextureId("NyanUI/other/fav_heart"))
+
+function Key(msg,code)
+	if msg ~= KEY_UP and code == toggleKey and not client.chat then
+		if not play then
+			play = true
+			rec1.visible = true
+			rec2.visible = true
+			return true
+		else
+			play = false
+			rec1.visible = false
+			rec2.visible = false
+			return true
+		end
+	end
+end
 
 function Tick(tick)
 	if not PlayingGame() then return end
 	local me = entityList:GetMyHero() if not me then return end
 	
-	if activated then	
-		if not rec1.visible then
-			rec1.visible = true
-			rec2.visible = true
-		end
+	local runes = entityList:GetEntities(function (ent) return ent.classId==CDOTA_Item_Rune and GetDistance2D(ent,me) < 200 end)[1]	
 
-		local runes = entityList:GetEntities(function (ent) return ent.classId==CDOTA_Item_Rune and GetDistance2D(ent,me) < 200 end)[1]		
-		if runes then 
-			entityList:GetMyPlayer():Select(me)
-			entityList:GetMyPlayer():TakeRune(runes)
-		end		
+	if play and runes then 
+		entityList:GetMyPlayer():Select(me)
+		entityList:GetMyPlayer():TakeRune(runes)	
 	end
 end
 
-function Key(msg,code)
-	if msg ~= KEY_UP and code == key and not client.chat then
-		activated = not activated
-	end
-end
 
 function Load()
 	if PlayingGame() then
@@ -48,12 +58,12 @@ end
 function Close()
 	collectgarbage("collect")
 	if play then
+		play = false
 		rec1.visible = false
 		rec2.visible = false
 		script:UnregisterEvent(Tick)
 		script:UnregisterEvent(Key)
 		script:RegisterEvent(EVENT_TICK,Load)
-		play = false
 	end
 end
 
